f88b0ac861c866a97c8e3b89e42e70719902a21e
puja-03 <pujakumari11th2003@gmail.com>
2025-10-30 15:23:08 +0530

    image upload successfully

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/', function (Request $request) {
+    $file = $request->file('image');
+    $path = Storage::putFile('youtube-tutorials', $file);
+    Storage::setVisibility($path, 'public');
+    return response()->json([
+        'path' => $path,
+        'url' => Storage::url($path)
+    ]);
+})->withoutMiddleware([VerifyCsrfToken::class]);
